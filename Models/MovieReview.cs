546608bac39b6c540eeaa8e718bc6e54fbf3f2af
HIRALI BANDHIYA <bandhiyahirali0608@gmail.com>
2026-05-19 12:35:13 +0530

    moviereview.cs page updates

--- Models/MovieReview.cs
+++ Models/MovieReview.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieMania.Models
 {
-    [Table("MovieReviews")]
+    [Table("MovieReview")]
     public class MovieReview
     {
         [Key]
